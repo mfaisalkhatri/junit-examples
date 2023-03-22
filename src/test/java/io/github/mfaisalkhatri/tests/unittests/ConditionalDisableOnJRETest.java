1b69b0f2bd4779d38de6a780592184a292a54064
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2023-03-22 16:47:19 +0530

    added tests for disabled on os and disabled for jre

---
+++ src/test/java/io/github/mfaisalkhatri/tests/unittests/ConditionalDisableOnJRETest.java
@@ -0,0 +1,37 @@
+package io.github.mfaisalkhatri.tests.unittests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledForJreRange;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
+
+/**
+ * @author Faisal Khatri
+ * @since 3/22/2023
+ **/
+public class ConditionalDisableOnJRETest {
+
+    @DisabledOnJre (JRE.JAVA_11)
+    @Test
+    public void unitTestOne() {
+        System.out.println ("This test is unit test one!!");
+    }
+
+    @DisabledForJreRange (min = JRE.JAVA_11)
+    @Test
+    public void unitTestTwo() {
+        System.out.println ("This test is unit test two!!");
+    }
+
+    @DisabledForJreRange (max= JRE.JAVA_15)
+    @Test
+    public void unitTestThree() {
+        System.out.println ("This test is unit test three!!");
+    }
+
+    @DisabledForJreRange (min = JRE.JAVA_9, max= JRE.JAVA_15)
+    @Test
+    public void unitTestFour() {
+        System.out.println ("This test is unit test four!!");
+    }
+}
---
+++ src/test/java/io/github/mfaisalkhatri/tests/unittests/ConditionalDisableOnOSTest.java
@@ -0,0 +1,32 @@
+package io.github.mfaisalkhatri.tests.unittests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
+
+/**
+ * @author Faisal Khatri
+ * @since 3/22/2023
+ **/
+public class ConditionalDisableOnOSTest {
+
+    @DisabledOnOs(value = OS.WINDOWS, disabledReason = "This feature is available for MAC users only")
+    @Test
+    public void unitTestOne() {
+        System.out.println ("This test is unit test one!!");
+    }
+    @DisabledOnOs(architectures = "x86_64", disabledReason =  "Feature not ready for x86_64 architecture")
+    @Test
+    public void unitTestTwo() {
+        System.out.println ("This test is unit test two!!");
+    }
+
+    @DisabledOnOs(value = OS.MAC, architectures = "AArch64", disabledReason = "Fix is not available for MAC")
+    @Test
+    public void unitTestThree() {
+        System.out.println ("This test is unit test three!!");
+    }
+
+}
