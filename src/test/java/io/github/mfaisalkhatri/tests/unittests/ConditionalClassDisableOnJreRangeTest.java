25d6ecdf22dd91042a7057f8a077833ead002c4d
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2023-03-25 17:29:40 +0530

    added example for disable tests on jre range at class level

---
+++ src/test/java/io/github/mfaisalkhatri/tests/unittests/ConditionalClassDisableOnJreRangeTest.java
@@ -0,0 +1,19 @@
+package io.github.mfaisalkhatri.tests.unittests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
+
+@DisabledForJreRange(min = JRE.JAVA_9, max = JRE.JAVA_15, disabledReason = "Tests disabled for JRE range 9 - 15")
+public class ConditionalClassDisableOnJreRangeTest {
+
+    @Test
+    public void unitTestOne() {
+        System.out.println("This is unit test one!!");
+    }
+
+    @Test
+    public void unitTestTwo() {
+        System.out.println("This is unit test two!!");
+    }
+}
