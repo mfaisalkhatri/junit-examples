9b90d519f9f74a53a51203c7dd8a34e81fe5df6d
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2023-03-25 15:09:05 +0530

    added tests for jre range example and disabled on os based test class

--- src/test/java/io/github/mfaisalkhatri/tests/unittests/ConditionalClassDisableOnJRETest.java
+++ src/test/java/io/github/mfaisalkhatri/tests/unittests/ConditionalClassDisableOnJRETest.java
@@ -8,15 +8,14 @@ import org.junit.jupiter.api.condition.JRE;
  * @author Faisal Khatri
  * @since 3/22/2023
  **/
-public class ConditionalDisableOnJRETest {
+@DisabledOnJre(value = JRE.JAVA_11, disabledReason = "This class is disabled when running on Java 11")
+public class ConditionalClassDisableOnJRETest {
 
-    @DisabledOnJre(value = JRE.JAVA_11, disabledReason = "JRE 11 support will be provided in next release, till time, test disabled")
     @Test
     public void unitTestOne() {
         System.out.println("This is unit test one!!");
     }
 
-    @DisabledOnJre(JRE.JAVA_15)
     @Test
     public void unitTestTwo() {
         System.out.println("This is unit test two");
