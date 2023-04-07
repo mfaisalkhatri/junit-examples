868164a3b7fe4026b0479676f29872c854c421dc
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2023-04-07 12:07:20 +0530

    added multiple browser support in drivermanager class

--- src/test/java/io/github/mfaisalkhatri/tests/LambdaTestEcommerceWebsiteTest.java
+++ src/test/java/io/github/mfaisalkhatri/tests/LambdaTestEcommerceWebsiteTest.java
@@ -1,26 +1,12 @@
 package io.github.mfaisalkhatri.tests;
 
-import static io.github.mfaisalkhatri.drivers.DriverManager.createDriver;
-import static io.github.mfaisalkhatri.drivers.DriverManager.getDriver;
-import static io.github.mfaisalkhatri.drivers.DriverManager.quitDriver;
+import io.github.mfaisalkhatri.drivers.Browsers;
+import io.github.mfaisalkhatri.lambdatestcommerce.pages.*;
+import org.junit.jupiter.api.*;
+
+import static io.github.mfaisalkhatri.drivers.DriverManager.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import io.github.mfaisalkhatri.lambdatestcommerce.pages.AccountPage;
-import io.github.mfaisalkhatri.lambdatestcommerce.pages.HomePage;
-import io.github.mfaisalkhatri.lambdatestcommerce.pages.LoginPage;
-import io.github.mfaisalkhatri.lambdatestcommerce.pages.RegistrationPage;
-import io.github.mfaisalkhatri.lambdatestcommerce.pages.RegistrationSuccessPage;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
 /**
  * @author Faisal Khatri
@@ -31,85 +17,85 @@ import org.junit.jupiter.api.TestMethodOrder;
 public class LambdaTestEcommerceWebsiteTest {
 
     private static String registeredEmail = null;
-    private static String password        = null;
+    private static String password = null;
 
     @BeforeAll
-    public static void testSetup () {
-        createDriver ();
+    public static void testSetup() {
+        createDriver(Browsers.CHROME);
         final String website = "https://ecommerce-playground.lambdatest.io/";
-        getDriver ().get (website);
+        getDriver().get(website);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        quitDriver();
     }
 
     @Test
-    @DisplayName ("Test by registering a new user")
-    public void testRegisterUser () {
-        HomePage homePage = new HomePage ();
-        RegistrationPage registrationPage = homePage.openUserRegistrationPage ();
-        registeredEmail = registrationPage.getRegisteredEmail ();
-        password = registrationPage.getPassword ();
-        assertEquals ("Register Account", registrationPage.getPageHeader ());
+    @DisplayName("Test by registering a new user")
+    public void testRegisterUser() {
+        HomePage homePage = new HomePage();
+        RegistrationPage registrationPage = homePage.openUserRegistrationPage();
+        registeredEmail = registrationPage.getRegisteredEmail();
+        password = registrationPage.getPassword();
+        assertEquals("Register Account", registrationPage.getPageHeader());
 
-        RegistrationSuccessPage registrationSuccessPage = registrationPage.registerUser ();
-        assertEquals ("Your Account Has Been Created!", registrationSuccessPage.getRegistrationSuccessMessage ());
-        AccountPage accountPage = new AccountPage ();
-        accountPage.logoutLink ()
-            .click ();
+        RegistrationSuccessPage registrationSuccessPage = registrationPage.registerUser();
+        assertEquals("Your Account Has Been Created!", registrationSuccessPage.getRegistrationSuccessMessage());
+        AccountPage accountPage = new AccountPage();
+        accountPage.logoutLink()
+                .click();
     }
 
     @Nested
-    @DisplayName ("Login Functionality Tests")
-    @TestMethodOrder (MethodOrderer.OrderAnnotation.class)
+    @DisplayName("Login Functionality Tests")
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     class Login {
 
         private LoginPage loginPage;
 
         @BeforeEach
-        public void testSetup () {
+        public void testSetup() {
             final String website = "https://ecommerce-playground.lambdatest.io/";
-            getDriver ().get (website);
-            HomePage homePage = new HomePage ();
-            loginPage = homePage.openLoginPage ();
+            getDriver().get(website);
+            HomePage homePage = new HomePage();
+            loginPage = homePage.openLoginPage();
         }
 
         @Test
-        @DisplayName ("Should not allow logging in user with Invalid Username")
-        @Order (1)
-        void testLoginWithInvalidUsername () {
-            loginPage.loginIntoWebsite ("test_invalid_username@gmail.com", password);
-            assertEquals ("Warning: No match for E-Mail Address and/or Password.", loginPage.getFailedLoginWarning ());
+        @DisplayName("Should not allow logging in user with Invalid Username")
+        @Order(1)
+        void testLoginWithInvalidUsername() {
+            loginPage.loginIntoWebsite("test_invalid_username@gmail.com", password);
+            assertEquals("Warning: No match for E-Mail Address and/or Password.", loginPage.getFailedLoginWarning());
         }
 
         @Test
-        @DisplayName ("Should not allow logging in user with Invalid Password")
-        @Order (2)
-        void testLoginWithInvalidPassword () {
-            loginPage.loginIntoWebsite (registeredEmail, "InvalidPassword");
-            assertEquals ("Warning: No match for E-Mail Address and/or Password.", loginPage.getFailedLoginWarning ());
-
-        }
-
-        @Test
-        @DisplayName ("Should not allow logging in user by keeping Username and Password fields blank")
-        @Order (3)
-        void testLoginWithBlankUsernameAndPassword () {
-            loginPage.loginIntoWebsite (" ", " ");
-            assertEquals ("Warning: No match for E-Mail Address and/or Password.", loginPage.getFailedLoginWarning ());
+        @DisplayName("Should not allow logging in user with Invalid Password")
+        @Order(2)
+        void testLoginWithInvalidPassword() {
+            loginPage.loginIntoWebsite(registeredEmail, "InvalidPassword");
+            assertEquals("Warning: No match for E-Mail Address and/or Password.", loginPage.getFailedLoginWarning());
 
         }
 
         @Test
-        @DisplayName ("Should allow logging in with Valid credentials")
-        @Order (4)
-        void testLoginWithValidCredentials () {
-            loginPage.loginIntoWebsite (registeredEmail, password);
-            AccountPage accountPage = new AccountPage ();
-            assertTrue (accountPage.logoutLink ()
-                .isDisplayed ());
-        }
-    }
+        @DisplayName("Should not allow logging in user by keeping Username and Password fields blank")
+        @Order(3)
+        void testLoginWithBlankUsernameAndPassword() {
+            loginPage.loginIntoWebsite(" ", " ");
+            assertEquals("Warning: No match for E-Mail Address and/or Password.", loginPage.getFailedLoginWarning());
 
-    @AfterAll
-    public static void tearDown () {
-        quitDriver ();
+        }
+
+        @Test
+        @DisplayName("Should allow logging in with Valid credentials")
+        @Order(4)
+        void testLoginWithValidCredentials() {
+            loginPage.loginIntoWebsite(registeredEmail, password);
+            AccountPage accountPage = new AccountPage();
+            assertTrue(accountPage.logoutLink()
+                    .isDisplayed());
+        }
     }
 }
--- src/test/java/io/github/mfaisalkhatri/tests/SeleniumPlaygroundTests.java
+++ src/test/java/io/github/mfaisalkhatri/tests/SeleniumPlaygroundTests.java
@@ -1,10 +1,6 @@
 package io.github.mfaisalkhatri.tests;
 
-import static io.github.mfaisalkhatri.drivers.DriverManager.createDriver;
-import static io.github.mfaisalkhatri.drivers.DriverManager.getDriver;
-import static io.github.mfaisalkhatri.drivers.DriverManager.quitDriver;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import io.github.mfaisalkhatri.drivers.Browsers;
 import io.github.mfaisalkhatri.seleniumplayground.pages.HomePage;
 import io.github.mfaisalkhatri.seleniumplayground.pages.SimpleFormPage;
 import org.junit.jupiter.api.AfterAll;
@@ -12,6 +8,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+
+import static io.github.mfaisalkhatri.drivers.DriverManager.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created By Faisal Khatri on 17-03-2023
@@ -22,35 +21,35 @@ public class SeleniumPlaygroundTests {
     private static HomePage homePage;
 
     @BeforeAll
-    public static void testSetup () {
-        homePage = new HomePage ();
-        createDriver ();
+    public static void testSetup() {
+        homePage = new HomePage();
+        createDriver(Browsers.SAFARI);
         final String website = "https://www.lambdatest.com/selenium-playground/";
-        getDriver ().get (website);
-    }
-
-    //@DisabledForJreRange (min = JAVA_15)
-    @DisabledOnOs (OS.WINDOWS)
-    @Test
-    public void testSimpleForm () {
-        homePage.navigateToLink ("Simple Form Demo");
-        SimpleFormPage formPage = new SimpleFormPage ();
-        String messageText = "This is a test";
-        formPage.enterMessage (messageText);
-        assertEquals (messageText, formPage.getMessage ());
-    }
-
-    @Test
-    public void testAdditionOfValues () {
-        homePage.navigateToLink ("Simple Form Demo");
-        SimpleFormPage formPage = new SimpleFormPage ();
-        formPage.getSumOfTwoNumbers ("2", "6");
-        assertEquals ("8", formPage.getResultText ());
+        getDriver().get(website);
     }
 
     @AfterAll
-    public static void tearDown () {
-        quitDriver ();
+    public static void tearDown() {
+        quitDriver();
+    }
+
+    @DisabledOnOs(OS.MAC)
+    @Test
+    public void testSimpleForm() {
+        homePage.navigateToLink("Simple Form Demo");
+        SimpleFormPage formPage = new SimpleFormPage();
+        final String messageText = "This is a test";
+        formPage.enterMessage(messageText);
+        assertEquals(messageText, formPage.getMessage());
+    }
+
+    @DisabledOnOs(OS.MAC)
+    @Test
+    public void testAdditionOfValues() {
+        homePage.navigateToLink("Simple Form Demo");
+        SimpleFormPage formPage = new SimpleFormPage();
+        formPage.getSumOfTwoNumbers("2", "6");
+        assertEquals("8", formPage.getResultText());
     }
 
 }
