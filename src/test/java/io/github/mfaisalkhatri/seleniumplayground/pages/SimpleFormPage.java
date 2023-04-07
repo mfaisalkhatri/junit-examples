391747fd38d13948c4c2449cc428367bbb3cbd66
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2023-04-07 16:54:13 +0530

    updated selenium tests

--- src/test/java/io/github/mfaisalkhatri/seleniumplayground/pages/SimpleFormPage.java
+++ src/test/java/io/github/mfaisalkhatri/seleniumplayground/pages/SimpleFormPage.java
@@ -1,52 +1,51 @@
 package io.github.mfaisalkhatri.seleniumplayground.pages;
 
-import static io.github.mfaisalkhatri.drivers.DriverManager.getDriver;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
+import static io.github.mfaisalkhatri.drivers.DriverManager.getDriver;
 
 /**
  * Created By Faisal Khatri on 20-03-2023
  */
 public class SimpleFormPage {
 
-    private WebElement messageField () {
-        return getDriver ().findElement (By.id ("user-message"));
+    public void getSumOfTwoNumbers(String numberOne, String numberTwo) {
+        fieldA().sendKeys(numberOne);
+        fieldB().sendKeys(numberTwo);
+        getValuesBtn().click();
     }
 
-    private WebElement showMessageBtn() {
-        return getDriver ().findElement (By.id ("showInput"));
+    private WebElement messageField() {
+        return getDriver().findElement(By.id("user-message"));
     }
 
     public String getMessage() {
-        return getDriver ().findElement (By.id ("message")).getText ();
+        return getDriver().findElement(By.id("message")).getText();
     }
 
-    public void enterMessage(String messageText) {
-        messageField ().sendKeys (messageText);
-        showMessageBtn ().click ();
+    private WebElement fieldB() {
+        return getDriver().findElement(By.id("sum2"));
     }
 
-    private WebElement fieldA() {
-        return getDriver ().findElement (By.id("sum1"));
-
-    }
-
-    private WebElement fieldB ( ) {
-        return getDriver ().findElement (By.id ("sum2"));
+    public String getResultText() {
+        return getDriver().findElement(By.id("addmessage")).getText();
     }
 
     private WebElement getValuesBtn() {
-        return getDriver ().findElement (By.cssSelector ("#gettotal > button"));
+        return getDriver().findElement(By.cssSelector("#gettotal > button"));
     }
 
-    public void getSumOfTwoNumbers(String numberOne, String numberTwo) {
-        fieldA ().sendKeys (numberOne);
-        fieldB ().sendKeys (numberTwo);
-        getValuesBtn ().click ();
+    private WebElement fieldA() {
+        return getDriver().findElement(By.id("sum1"));
     }
 
-    public String getResultText () {
-        return getDriver ().findElement (By.id("addmessage")).getText ();
+    private WebElement showMessageBtn() {
+        return getDriver().findElement(By.id("showInput"));
+    }
+
+    public void enterMessage(String messageText) {
+        messageField().sendKeys(messageText);
+        showMessageBtn().click();
     }
 }
--- src/test/java/io/github/mfaisalkhatri/tests/seleniumautomationtests/SeleniumPlaygroundTests.java
+++ src/test/java/io/github/mfaisalkhatri/tests/seleniumautomationtests/SeleniumPlaygroundTests.java
@@ -4,7 +4,6 @@ import io.github.mfaisalkhatri.drivers.Browsers;
 import io.github.mfaisalkhatri.seleniumplayground.pages.HomePage;
 import io.github.mfaisalkhatri.seleniumplayground.pages.SimpleFormPage;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
@@ -17,39 +16,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 
 public class SeleniumPlaygroundTests {
-
-    private static HomePage homePage;
-
-    @BeforeAll
-    public static void testSetup() {
-        homePage = new HomePage();
-        createDriver(Browsers.SAFARI);
-        final String website = "https://www.lambdatest.com/selenium-playground/";
-        getDriver().get(website);
-    }
-
     @AfterAll
     public static void tearDown() {
         quitDriver();
     }
 
-    @DisabledOnOs(OS.MAC)
+    @DisabledOnOs(value = OS.MAC, architectures = "aarch64")
     @Test
     public void testSimpleForm() {
+        createDriver(Browsers.SAFARI);
+        final String website = "https://www.lambdatest.com/selenium-playground/";
+        getDriver().get(website);
+
+        HomePage homePage = new HomePage();
         homePage.navigateToLink("Simple Form Demo");
+
         SimpleFormPage formPage = new SimpleFormPage();
         final String messageText = "This is a test";
         formPage.enterMessage(messageText);
         assertEquals(messageText, formPage.getMessage());
     }
 
-    @DisabledOnOs(OS.MAC)
+    @DisabledOnOs(OS.WINDOWS)
     @Test
     public void testAdditionOfValues() {
+
+        createDriver(Browsers.CHROME);
+        final String website = "https://www.lambdatest.com/selenium-playground/";
+        getDriver().get(website);
+
+        HomePage homePage = new HomePage();
         homePage.navigateToLink("Simple Form Demo");
+
         SimpleFormPage formPage = new SimpleFormPage();
         formPage.getSumOfTwoNumbers("2", "6");
         assertEquals("8", formPage.getResultText());
+        //to find os arch
+        //System.out.println(System.getProperty("os.arch"));
+
     }
 
 }
