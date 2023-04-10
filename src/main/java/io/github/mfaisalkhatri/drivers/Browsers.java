cc0b36e7c7bf3deb550d1de421fc931841ea1de8
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2023-04-10 12:16:13 +0530

    Added selenium tests for demonstrating disabled tests (#8)
    * added multiple browser support in drivermanager class
    
    * updated selenium tests
    
    * added multiple browser support for running selenium tests

---
+++ src/main/java/io/github/mfaisalkhatri/drivers/Browsers.java
@@ -0,0 +1,30 @@
+package io.github.mfaisalkhatri.drivers;
+
+public enum Browsers {
+
+    /**
+     * Chrome Browser
+     */
+    CHROME,
+    /**
+     * Edge browser.
+     */
+    EDGE,
+    /**
+     * Firefox browser.
+     */
+    FIREFOX,
+    /**
+     * Safari browser.
+     */
+    SAFARI,
+    /**
+     * Internet Explorer browser.
+     */
+    IE,
+
+    /**
+     * Chrome browser in Remote(LambdaTest Platform).
+     */
+    REMOTE_CHROME
+}
